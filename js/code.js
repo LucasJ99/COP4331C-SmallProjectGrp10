ee002328f69682cb3ec67691e96d30d5f031d647
BryceMcIn <55037125+BryceMcIn@users.noreply.github.com>
2021-01-28 12:30:57 -0500

    Create code.js

---
+++ js/code.js
@@ -0,0 +1,256 @@
+var urlBase = 'http://contactlookup.ninja/LAMPAPI';
+var extension = 'php';
+
+var userId = 0;
+var firstName = "";
+var lastName = "";
+
+function doRegister()
+{
+  	document.getElementById("registerResult").innerHTML = "";
+
+	var firstName = document.getElementById("registrationFirstName").value;
+	var lastName = document.getElementById("registrationLastName").value;
+	var username = document.getElementById("registrationLogin").value;
+	var password = document.getElementById("registrationPassword").value;
+
+	if(password != document.getElementById("registrationRetypedPassword").value){
+		document.getElementById("registerResult").innerHTML = "Passwords do not match!";
+		return;
+	}
+	
+	document.getElementById("registerResult").innerHTML = "";
+	
+	var jsonPayload = '{"Login" : "' + username + '", "FirstName" : "' + firstName + '", "LastName" : "' + lastName + '", "Password" : "' + password + '"}';
+	var url = urlBase + '/CreateAccount.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try{
+		xhr.send(jsonPayload);
+	}
+	catch(err){
+		document.getElementById("registerResult").innerHTML = err.message;
+	}
+	
+	if(document.getElementById("registerResult").innerHTML == ""){
+		window.location.href = "index.html";
+		document.getElementById("loginResult").innerHTML = "Registration successful. Please sign in";
+	}
+	return false;
+}
+
+function doAlert()
+{
+	window.alert("alert");
+}
+
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	var login = document.getElementById("loginUsername").value;
+	var password = document.getElementById("loginPassword").value;
+//	var hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
+
+//	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+	var jsonPayload = '{"Login" : "' + login + '", "Password" : "' + password + '"}';
+	var url = urlBase + '/Login.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.send(jsonPayload);
+		
+		var jsonObject = JSON.parse( xhr.responseText );
+		
+		userId = jsonObject.ID;
+		if( userId < 1 )
+		{
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+		
+		firstName = jsonObject.FirstName;
+		lastName = jsonObject.LastName;
+		saveCookie();
+
+		window.location.href = "color.html";
+		return false;
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+
+
+//testing login
+function saveCookie()
+{
+	var minutes = 20;
+	var date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
+
+function addContact()
+{
+
+	//add address field to webpage
+	//add addContactResult to webpage
+	var FirstName = document.getElementById("addFirstName").value;
+	var LastName = document.getElementById("addLastName").value;
+	var Email = document.getElementById("addEmail").value;
+	var PhoneNumber = document.getElementById("addPhoneNumber").value;
+	var Address = document.getElementById("addAddress").value;
+
+	document.getElementById("colorAddResult").innerHTML = "";
+
+	var jsonPayload = '{"UserID" : "' + userId + '", "FirstName" : "' + FirstName + '", "LastName" : "' + LastName + '", "PhoneNumber" : "' +PhoneNumber+ '", "Email" : "' +Email+ '"}'
+
+	var url = urlBase + '/AddContact.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200){
+				document.getElementById("addContactResult").innerHTML = "Contact has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactAddResult").innerHTML = err.message;
+	}
+
+}
+
+function addColor()
+{
+	var newColor = document.getElementById("colorText").value;
+	document.getElementById("colorAddResult").innerHTML = "";
+	
+	var jsonPayload = '{"color" : "' + newColor + '", "userId" : ' + userId + '}';
+	var url = urlBase + '/AddColor.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorAddResult").innerHTML = err.message;
+	}
+	
+}
+
+function searchColor()
+{
+	var srch = document.getElementById("searchText").value;
+	document.getElementById("colorSearchResult").innerHTML = "";
+	
+	var colorList = "";
+	
+	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
+	var url = urlBase + '/SearchColors.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+				var jsonObject = JSON.parse( xhr.responseText );
+				
+				for( var i=0; i<jsonObject.results.length; i++ )
+				{
+					colorList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						colorList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = colorList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorSearchResult").innerHTML = err.message;
+	}
+	
+}
