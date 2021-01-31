b26fc488cf645f669f9254cea9e8ff3082038afe
trevjones1121 <77754218+trevjones1121@users.noreply.github.com>
2021-01-31 14:18:19 -0500

    Add files via upload

--- SearchContacts.php
+++ SearchContacts.php
@@ -1,61 +1,61 @@
 <?php
 
-	$inData = getRequestInfo();
-	
-	$searchResults = "";
-	$searchCount = 0;
+        $inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "Obelisk", "TheTormentorOfCOP4331!", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$sql = "select FirstName,LastName from Contacts where (FirstName like '%" . $inData["search"] . "%' or LastName like '%" . $inData["search"] . "%') and UserID=" . $inData["userId"];
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0)
-		{
-			while($row = $result->fetch_assoc())
-			{
-				if( $searchCount > 0 )
-				{
-					$searchResults .= ",";
-				}
-				$searchCount++;
-				$searchResults .= '"' . $row["FirstName"] .' ' . $row["LastName"] . '"';
-			}
-		}
-		else
-		{
-			returnWithError( "No Records Found" );
-		}
-		$conn->close();
-	}
+        $searchResults = "";
+        $searchCount = 0;
 
-	returnWithInfo( $searchResults );
+        $conn = new mysqli("localhost", "Obelisk", "TheTormentorOfCOP4331!", "COP4331");
+        if ($conn->connect_error)
+        {
+                returnWithError( $conn->connect_error );
+        }
+        else
+        {
+                $sql = "select FirstName,LastName,ID from Contacts where (FirstName like '%" . $inData["search"] . "%' or LastName like '%" . $inData["search"] . "%') and UserID=" . $inData["userId"];
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0)
+                {
+                        while($row = $result->fetch_assoc())
+                        {
+                                if( $searchCount > 0 )
+                                {
+                                        $searchResults .= ",";
+                                }
+                                $searchCount++;
+                                $searchResults .= '"' . $row["FirstName"] .' ' . $row["LastName"] . ' ' . $row["ID"] . '"';
+                        }
+                }
+                else
+                {
+                        returnWithError( "No Records Found" );
+                }
+                $conn->close();
+        }
+		returnWithInfo( $searchResults );
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+        function sendResultInfoAsJson( $obj )
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
+
+        function returnWithError( $err )
+        {
+                $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+                sendResultInfoAsJson( $retValue );
+        }
+
+        function returnWithInfo( $searchResults )
+        {
+                $retValue = '{"results":[' . $searchResults . '],"error":""}';
+                sendResultInfoAsJson( $retValue );
+        }
+
 ?>
+                                  
