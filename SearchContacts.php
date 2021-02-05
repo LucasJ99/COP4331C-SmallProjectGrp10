7cf4bcdcebf6623060424fa5f3210a4401a184d0
LucasJ99 <71403530+LucasJ99@users.noreply.github.com>
2021-02-05 14:35:03 -0500

    Fixed error handling
    2 JSON packages were returning when there was an error - now only 1 package returns.

--- SearchContacts.php
+++ SearchContacts.php
@@ -1,61 +1,62 @@
 <?php
 
-        $inData = getRequestInfo();
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
 
-        $searchResults = "";
-        $searchCount = 0;
+	$conn = new mysqli("localhost", "Obelisk", "TheTormentorOfCOP4331!", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "select FirstName,LastName,ID from Contacts where (FirstName like '%" . $inData["search"] . "%' or LastName like '%" . $inData["search"] . "%') and UserID=" . $inData["userId"];
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0)
+		{
+			while($row = $result->fetch_assoc())
+			{
+				if( $searchCount > 0 )
+				{
+					$searchResults .= ",";
+				}
+				$searchCount++;
+				$searchResults .= '"' . $row["FirstName"] .' ' . $row["LastName"] . ' ' . $row["ID"] . '"';
+			}
+			returnWithInfo( $searchResults );
+		}
+		else
+		{
+			returnWithError( "No Records Found" );
+		}
+		$conn->close();
+	}
 
-        $conn = new mysqli("localhost", "Obelisk", "TheTormentorOfCOP4331!", "COP4331");
-        if ($conn->connect_error)
-        {
-                returnWithError( $conn->connect_error );
-        }
-        else
-        {
-                $sql = "select FirstName,LastName,ID from Contacts where (FirstName like '%" . $inData["search"] . "%' or LastName like '%" . $inData["search"] . "%') and UserID=" . $inData["userId"];
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0)
-                {
-                        while($row = $result->fetch_assoc())
-                        {
-                                if( $searchCount > 0 )
-                                {
-                                        $searchResults .= ",";
-                                }
-                                $searchCount++;
-                                $searchResults .= '"' . $row["FirstName"] .' ' . $row["LastName"] . ' ' . $row["ID"] . '"';
-                        }
-                }
-                else
-                {
-                        returnWithError( "No Records Found" );
-                }
-                $conn->close();
-        }
-		returnWithInfo( $searchResults );
+	// returnWithInfo( $searchResults );
 
-        function getRequestInfo()
-        {
-                return json_decode(file_get_contents('php://input'), true);
-        }
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
-        function sendResultInfoAsJson( $obj )
-        {
-                header('Content-type: application/json');
-                echo $obj;
-        }
-
-        function returnWithError( $err )
-        {
-                $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-                sendResultInfoAsJson( $retValue );
-        }
-
-        function returnWithInfo( $searchResults )
-        {
-                $retValue = '{"results":[' . $searchResults . '],"error":""}';
-                sendResultInfoAsJson( $retValue );
-        }
-
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
-                                  
