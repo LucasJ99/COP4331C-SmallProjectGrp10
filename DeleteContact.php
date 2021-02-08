dd8a2f1e1cf5885a592158cf32855313b82d538f
trevjones1121 <77754218+trevjones1121@users.noreply.github.com>
2021-02-08 18:11:26 -0500

    Add files via upload

--- DeleteContact.php
+++ DeleteContact.php
@@ -1,42 +1,40 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$UserID = $inData["UserID"];
-	$FirstName = $inData["FirstName"];
-	$LastName = $inData["LastName"];
-	
-	$conn = new mysqli("localhost", "Obelisk", "TheTormentorOfCOP4331!", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$sql = "DELETE FROM Contacts WHERE UserID=" . $UserID . " AND  FirstName='" . $FirstName . "' AND LastName='" . $LastName . "'";
-		$result = $conn->query($sql);
-		if( $result = $conn->query($sql) != TRUE )
-		{
-			returnWithError( $conn->error );
-		}
-		$conn->close();
-	}
-	
-	returnWithError("");
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        $inData = getRequestInfo();
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+        $ID = $inData["ID"];
+
+        $conn = new mysqli("localhost", "Obelisk", "TheTormentorOfCOP4331!", "COP4331");
+        if ($conn->connect_error)
+        {
+                returnWithError( $conn->connect_error );
+        }
+        else
+        {
+                $sql = "DELETE FROM Contacts WHERE ID=" . $ID ."";
+                $result = $conn->query($sql);
+                if( $result = $conn->query($sql) != TRUE )
+                {
+                        returnWithError( $conn->error );
+                }
+                $conn->close();
+        }
+
+        returnWithError("");
+
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
+
+        function sendResultInfoAsJson( $obj )
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
+
+        function returnWithError( $err )
+        {
+                $retValue = '{"error":"' . $err . '"}';
+                sendResultInfoAsJson( $retValue );
+        }
 ?>
